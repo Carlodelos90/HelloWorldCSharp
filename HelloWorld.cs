c34fce9d6e68f2f5293d378e92985d50665ea160
Carlo <carlodelos90@gmail.com>
2024-11-07 21:27:58 +0100

    Hello world in C#

---
+++ HelloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class HelloWorld {
+    public static void Main() {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+
+/*
+Explanation: In C#, using System; imports the System namespace, which includes Console. Console.WriteLine() outputs text to the console, and WriteLine adds a newline after printing.
+*/
